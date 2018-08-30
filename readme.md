ee87d8cc13f328fa21054264877a3438675e926d
jdiperi88 <jdiperi88@yahoo.com>
2018-08-29 22:42:07 -0400

    made first home component

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # React Server Side Rendering
 
-- This repo is to familarize with leveraging server side rendering with react.
+- This repo is to familarize myself with leveraging server side rendering with react.
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const React = require("react");
+const renderToString = require("react-dom/server").renderToString;
+const Home = require("./client/components/Home").default;
+
+app.get("/", (req, res) => {
+	const content = renderToString(<Home />);
+
+	res.send(content);
+});
+
+const port = 3000;
+app.listen(port, () => {
+	console.log(`listening on port: ${port}`);
+});
