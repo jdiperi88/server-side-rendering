09d42b51508760145475aa14fd230e5ffe238f6d
jdiperi88 <jdiperi@Josephs-MBP-2.fios-router.home>
2019-01-29 19:52:19 -0500

    minor style change

--- webpack.server.js
+++ webpack.server.js
@@ -1,33 +1,33 @@
 const path = require("path");
 
 module.exports = {
-	//inform webpack that were building a bundle for node js
-	target: "node",
+  //inform webpack that were building a bundle for node js
+  target: "node",
 
-	// tell webpack the root file of our server app
-	entry: "./src/index.js",
+  // tell webpack the root file of our server app
+  entry: "./src/index.js",
 
-	//tell webpack where to put the output file that is generated
-	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "build")
-	},
+  //tell webpack where to put the output file that is generated
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
 
-	//tell webpack to run babel on every file it runs through
-	module: {
-		rules: [
-			{
-				test: /\.js?$/,
-				loader: "babel-loader",
-				exclude: /node_modules/,
-				options: {
-					presets: [
-						"react",
-						"stage-0",
-						["env", { targets: { browsers: ["last 2 versions"] } }]
-					]
-				}
-			}
-		]
-	}
+  //tell webpack to run babel on every file it runs through
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            "react",
+            "stage-0",
+            ["env", { targets: { browsers: ["last 2 versions"] } }]
+          ]
+        }
+      }
+    ]
+  }
 };
